6c5d602cb8f23e3883a7eefa75e1df5c35f8fc42
Nandu Dumare <dumarenandu70@gmail.com>
2022-08-23 20:27:10 +0530

    resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,14 +1,13 @@
-import GreyResume from '../assets/pdf/GreyResume.pdf';
+import resume from "../assets/pdf/resume.pdf";
 // import img from "../assets/Picture/DSC_8868.JPG";
 import img from "../assets/Picture/Crop img.jpg";
-import ResumeGrid from "../assets/pdf/ResumeGrid.pdf"
-
-
+import ResumeGrid from "../assets/pdf/ResumeGrid.pdf";
 
 export const headerData = {
-    name: 'Nandkishor Dumare',
-    title: "Full Stack Web Developer",
-    desciption: "I want to utilize my skills for development and work in a highly competitive environment to seek a new situation, pursue adaptability, create exceptional value and maximize my share of experience thus developing a phenomenally successful attitude towards living on the edge and becoming globally in demand.",
-    image: img,
-    resumePdf: ResumeGrid
-}
+  name: "Nandkishor Dumare",
+  title: "Full Stack Web Developer",
+  desciption:
+    "I want to utilize my skills for development and work in a highly competitive environment to seek a new situation, pursue adaptability, create exceptional value and maximize my share of experience thus developing a phenomenally successful attitude towards living on the edge and becoming globally in demand.",
+  image: img,
+  resumePdf: resume,
+};
