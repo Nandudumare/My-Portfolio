2307be2b72b28c43cd8c69f20182ae12c522652c
Nandu Dumare <dumarenandu70@gmail.com>
2022-09-17 19:31:16 +0530

    about-us

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,8 +1,8 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name is Nandkishor Dumare. I am from Takalghat, a beautiful village, which is 40 KMS away from Nagpur.",
+    "My name is Nandkishor Dumare. I'm a Full Stact Web Developer based in Nagpur, India",
   description2:
-    "I have completed my SSC from Srimati saraswatibai nistane vidyalaya with 84%. In H.S.C, I have got 81% after passing HSC, I was a little confused. I didn't know which profession I should go. In some corner of my heart, I was thinking of doing poly. And in second mind, it was engineering going in my mind. I was completely blocked at that time. But after a few days, I heard about coding. One of my friends told me coding is our future. Why not to join coding classes ? In fact we have a better school for this profession which is Masai School. At least, we try this course. It is in demand. What say? I agreed to it and joined Masai school. And I am proud of myself that I was at the right decision. And talking about my interest - I am fond of learning new things and reading quotes.",
+    "I'm passionate about learning new technologies and building projects.I have completed Full Stack Web Development Course at Masai School, Bengaluru, Karnataka. I love to learn and explore new web technologies. My interests and hobbies revolves around Coding and learning new things.",
   image: 1,
 };
