7668e8757a5e9f890688a14088ca677b056f1e7a
Nandu Dumare <dumarenandu70@gmail.com>
2022-09-17 19:32:02 +0530

    about-us

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name is Nandkishor Dumare. I'm a Full Stact Web Developer based in Nagpur, India",
+    "My name is Nandkishor Dumare. I'm a Full Stact Web Developer based in Nagpur, India.",
   description2:
     "I'm passionate about learning new technologies and building projects.I have completed Full Stack Web Development Course at Masai School, Bengaluru, Karnataka. I love to learn and explore new web technologies. My interests and hobbies revolves around Coding and learning new things.",
   image: 1,
